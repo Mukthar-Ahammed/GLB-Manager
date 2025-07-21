e415d624c0295cc07740d84552c7108644c2975f
Mukthar Ahammed <167291147+Mukthar-Ahammed@users.noreply.github.com>
2025-07-21 09:43:44 +0530

    Update db.js

--- Backend/lib/db.js
+++ Backend/lib/db.js
@@ -1,14 +1,18 @@
-import mongoose from 'mongoose'
-import dotenv from  'dotenv'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
-dotenv.config()
+dotenv.config();
 
-export const connectDb = async()=>{
-    try{
-        await mongoose.connect(process.env.MONGO_URI)
-        console.log("database connected")
-
-    }catch (error) {
-        console.log("error connecting the database",error)
-    }
-}
+export const connectDb = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Database connected successfully");
+  } catch (error) {
+    console.error("Error connecting to database:", error.message);
+  
+    setTimeout(connectDb, 5000); 
+  }
+};
